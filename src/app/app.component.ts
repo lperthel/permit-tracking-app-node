c6c9c45d15137a28c2f7b4ec0f264b5b13d16caa
Leland Perthel <lperthel@gmail.com>
2025-07-01 10:05:34 -0400

    Downgraded faker to fix security problems. Removed unnedded import from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AllProductsComponent } from './products/all-products/all-products.compo
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AllProductsComponent],
+  imports: [AllProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
